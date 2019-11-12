c17928bae114625c91bfa8c07534d6ff22f31786
kirari_kiis <kirari0709@icloud.com>
2019-11-13 00:45:38 +0900

    login_css 更新

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,8 @@ Rails.application.routes.draw do
   get 'home/top'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
+    :sessions => 'users/sessions' ,
+    :passwords => 'users/passwords'  
   } 
   
   resources :users
